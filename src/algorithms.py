c0c3f75515b24e23dc3215eb691640a2e5b70cb7
acuzumm <aliceemuzum@gmail.com>
2022-05-24 17:33:22 +0300

    algorithms.py upgrade

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,20 +1,17 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
+import os 
 
 class Algorithms():
     
-    def get_contour_points(self,image: np.ndarray) -> np.ndarray:
+    def get_all_contours(self,image=None):
         """
-        Returns contours of the given image.
-
-        Parameters:
-
-        image (np.ndarray): Single image to calculating the contours.
+        Returns all of the contours from the dataset.
 
         Returns:
 
-        np.ndarray: It is shaped as (x,y,z,t). x for amount of image sent usually 1, y for amount of contour points, z for how many elements in one contour point , t for coordinate dimension of the point, 
+        list: It is shaped as (x,y,z,t). x for amount of image sent usually 1, y for amount of contour points, z for how many elements in one contour point , t for coordinate dimension of the point, 
         e.g., 
         
         shape = (1, 3, 1, 2)
@@ -27,17 +24,30 @@ class Algorithms():
 
                     ]]] . 
         """
-        #Converting to grayscale and then to binary image
-        raw_gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        th, bin_img = cv.threshold(raw_gray_img, 127, 255, cv.THRESH_OTSU)
-        des = cv.bitwise_not(bin_img)
-        _,contour,_ = cv.findContours(des,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
-        contour = np.array(contour)
-        return contour
+        if image is not None:
+            raw_gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+            th, bin_img = cv.threshold(raw_gray_img, 1, 255, cv.THRESH_OTSU)
+            des = cv.bitwise_not(bin_img)
+            _,cnts,_ = cv.findContours(des,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
+            return cnts
+        else:
+            contours = []
+            DIR = "../dataset/cut"
+            for f in os.listdir(DIR):
+                f = os.path.join(DIR,f)
+                image = cv.imread(f)
+                #Converting to grayscale and then to binary image
+                raw_gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+                th, bin_img = cv.threshold(raw_gray_img, 1, 255, cv.THRESH_OTSU)
+                des = cv.bitwise_not(bin_img)
+                _,cnts,_ = cv.findContours(des,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
+                #contour = np.array(contour)
+                contours.append(cnts)
+            return contours
 
-    def get_poly_points(self, image: np.ndarray,epsilon=0.01) -> np.ndarray:
+    def get_all_poly_points(self,image=None,epsilon=0.01) -> np.ndarray:
         """
-        Returns polynomial approximation points of the given image.
+        Returns all polynomial approximation points from the dataset.
 
         Parameters:
 
@@ -46,7 +56,7 @@ class Algorithms():
 
         Returns:
 
-        np.ndarray: It is shaped as (x,y,z). X for how many points for polynomial approximation, Y for how many coordinates for one point, Z for coordinate dimension
+        list: It is shaped as (x,y,z). X for how many points for polynomial approximation, Y for how many coordinates for one point, Z for coordinate dimension
         e.g., 
         
         shape = (3,1,2)
@@ -59,10 +69,19 @@ class Algorithms():
 
                     ]]] . 
         """
-        contours = self.get_contour_points(image)
-        for cnt in contours:
-            epsilon = epsilon*cv.arcLength(cnt, True)
-            approx = cv.approxPolyDP(cnt, epsilon, True)
-
-        return np.array(approx)
+        if image is not None:
+            contours = self.get_all_contours(image)
+            for cnt in contours:     
+                epsilon = epsilon*cv.arcLength(cnt, True)
+                approx = cv.approxPolyDP(cnt, epsilon, True)
+            return approx
+        else:
+            all_polys = []
+            contours = self.get_all_contours()
+            for cnt in contours:
+                cnt = np.array(cnt)
+                epsilon = epsilon*cv.arcLength(cnt, True)
+                approx = cv.approxPolyDP(cnt, epsilon, True)
+                all_polys.append(approx)
+            return all_polys
             
---
+++ src/exercises.py
@@ -0,0 +1,85 @@
+from tokenize import String
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+from algorithms import Algorithms
+import os
+
+# ----------UNCOMMEND THE EXERCISE YOU WISH-----------
+
+
+#****** Getting all contours and polys at once ******
+# Alg = Algorithms()
+# contours = Alg.get_all_contours()
+# print("Type of contours =             ",type(contours),"                  Length of contours =             ", len(contours), " num of calculated images ")
+# print("Type of contours[0] =          ",type(contours[0]), "                  Length of contours[0] =          ", len(contours[0]), "   how many objects in that image")
+# print("Type of contours[0][0] =       ",type(contours[0][0]), "         Length of contours[0][0] =       ", len(contours[0][0]),"  how many contour points ")
+# print("Type of contours[0][0][0] =    ",type(contours[0][0][0]), "         Length of contours[0][0][0] =    ", len(contours[0][0][0]), "   how many elements in one contour point")
+# print("Type of contours[0][0][0][0] = ",type(contours[0][0][0][0]), "         Length of contours[0][0][0][0] = ", len(contours[0][0][0][0]),"   coordination points")
+
+
+# polys = Alg.get_all_poly_points()
+# print("Type of polys = ",type(polys), "   Length of polys = ", len(polys))
+
+
+
+
+# ******** Example of polygonomial approximation.********
+img = cv.imread("../dataset/cut/obj_999.png")
+Alg = Algorithms()
+contours = Alg.get_all_contours(image=img)
+
+img_1 = np.zeros([100,100,1],dtype=np.uint8)
+img_2 = np.zeros([100,100,1],dtype=np.uint8)
+img_3 = np.zeros([100,100,1],dtype=np.uint8)
+img_4 = np.zeros([100,100,1],dtype=np.uint8)
+img_1.fill(255)
+img_2.fill(255)
+img_3.fill(255)
+img_4.fill(255)
+
+for cnt in contours:
+    approx = Alg.get_all_poly_points(img)
+    print(type(approx), approx.shape)
+    cv.drawContours(img_1, [approx], 0, (0), 1)
+    hull = cv.convexHull(cnt)
+    cv.drawContours(img_2, [hull], 0, (0), 1)
+
+    approx = Alg.get_all_poly_points(img,0.02)
+    cv.drawContours(img_3, [approx], 0, (0), 1)
+
+fig, axs = plt.subplots(nrows=2,ncols=2)
+axs[0,0].set_title("Poly Aprx. | Epsilon = 0.01")
+axs[0,1].set_title("Poly Aprx. | Epsilon = 0.02")
+axs[1,0].set_title("Convex Hull")
+axs[1,1].set_title("Original -> Not Contour")
+axs[0,0].imshow(img_1)
+axs[1,0].imshow(img_2)
+axs[0,1].imshow(img_3)
+axs[1,1].imshow(img)
+
+plt.show()
+
+
+
+
+
+
+
+
+# ********Adjusting all data to the same format*******
+# i = 999
+# for f in os.listdir("../dataset/cut"):
+#     f = os.path.join("../dataset/cut",f)
+    
+#     img = cv.imread(f)
+#     raw_gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#     th, bin_img = cv.threshold(raw_gray_img, 1, 255, cv.THRESH_OTSU)
+#     des = cv.bitwise_not(bin_img)
+#     _,contour,_ = cv.findContours(des,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
+#     for cnt in contour:
+#         cv.drawContours(des,[cnt],0,255,-1)
+    
+#     des = cv.bitwise_not(des)
+#     cv.imwrite("../dataset/cut/obj_" + str(i) + ".png", des)
+#     i -= 1
