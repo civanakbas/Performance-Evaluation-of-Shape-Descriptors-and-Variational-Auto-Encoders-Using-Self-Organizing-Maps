b5a6bd8248b760269e391f89a43b0f8952fc8fc6
civanakbas <civan.akbas@hotmail.com>
2022-05-24 18:10:39 +0300

    formatting

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
-import os 
+import os
 import math
 
-class Algorithms():
-    
-    def get_all_contours(self,image=None):
+
+class Algorithms:
+    def get_all_contours(self, image=None):
         """
         Returns all of the contours from the dataset.
 
@@ -29,24 +29,24 @@ class Algorithms():
             raw_gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
             th, bin_img = cv.threshold(raw_gray_img, 1, 255, cv.THRESH_OTSU)
             des = cv.bitwise_not(bin_img)
-            _,cnts,_ = cv.findContours(des,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
+            _, cnts, _ = cv.findContours(des, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
             return cnts
         else:
             contours = []
             DIR = "../dataset/cut"
             for f in os.listdir(DIR):
-                f = os.path.join(DIR,f)
+                f = os.path.join(DIR, f)
                 image = cv.imread(f)
-                #Converting to grayscale and then to binary image
+                # Converting to grayscale and then to binary image
                 raw_gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
                 th, bin_img = cv.threshold(raw_gray_img, 1, 255, cv.THRESH_OTSU)
                 des = cv.bitwise_not(bin_img)
-                _,cnts,_ = cv.findContours(des,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
-                #contour = np.array(contour)
+                _, cnts, _ = cv.findContours(des, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+                # contour = np.array(contour)
                 contours.append(cnts)
             return contours
 
-    def get_all_poly_points(self,image=None,epsilon=0.01) -> np.ndarray:
+    def get_all_poly_points(self, image=None, epsilon=0.01) -> np.ndarray:
         """
         Returns all polynomial approximation points from the dataset.
 
@@ -72,8 +72,8 @@ class Algorithms():
         """
         if image is not None:
             contours = self.get_all_contours(image)
-            for cnt in contours:     
-                epsilon = epsilon*cv.arcLength(cnt, True)
+            for cnt in contours:
+                epsilon = epsilon * cv.arcLength(cnt, True)
                 approx = cv.approxPolyDP(cnt, epsilon, True)
             return approx
         else:
@@ -81,11 +81,11 @@ class Algorithms():
             contours = self.get_all_contours()
             for cnt in contours:
                 cnt = np.array(cnt)
-                epsilon = epsilon*cv.arcLength(cnt, True)
+                epsilon = epsilon * cv.arcLength(cnt, True)
                 approx = cv.approxPolyDP(cnt, epsilon, True)
                 all_polys.append(approx)
             return all_polys
-            
+
     def _calculate_compass_bearing(self, pt1, pt2):
         """
         Returns the bearing between two points.
@@ -104,15 +104,15 @@ class Algorithms():
         diffLong = math.radians(pt2[1] - pt1[1])
 
         x = math.sin(diffLong) * math.cos(lat2)
-        y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
-                * math.cos(lat2) * math.cos(diffLong))
+        y = math.cos(lat1) * math.sin(lat2) - (
+            math.sin(lat1) * math.cos(lat2) * math.cos(diffLong)
+        )
 
         initial_bearing = math.atan2(x, y)
         initial_bearing = math.degrees(initial_bearing)
         compass_bearing = (initial_bearing + 360) % 360
 
         return compass_bearing
-        
 
     def _check_direction(self, degree):
         """
@@ -139,14 +139,10 @@ class Algorithms():
         histogram = [0] * 8
         contours = self.get_all_contours(image)
         contours = np.array(contours)
-        contours = contours.reshape(contours.shape[1],2)
+        contours = contours.reshape(contours.shape[1], 2)
 
         for idx in range(len(contours) - 1):
-            degree = self._calculate_compass_bearing(contours[idx], contours[idx+1])
+            degree = self._calculate_compass_bearing(contours[idx], contours[idx + 1])
             histogram[self._check_direction(degree)] += 1
 
         return histogram
-
-
-
-    
--- src/exercises.py
+++ src/exercises.py
@@ -8,7 +8,7 @@ import os
 # ----------UNCOMMEND THE EXERCISE YOU WISH-----------
 
 
-#****** Getting all contours and polys at once ******
+# ****** Getting all contours and polys at once ******
 # Alg = Algorithms()
 # contours = Alg.get_all_contours()
 # print("Type of contours =             ",type(contours),"                  Length of contours =             ", len(contours), " num of calculated images ")
@@ -22,17 +22,15 @@ import os
 # print("Type of polys = ",type(polys), "   Length of polys = ", len(polys))
 
 
-
-
 # ******** Example of polygonomial approximation.********
 img = cv.imread("../dataset/cut/obj_999.png")
 Alg = Algorithms()
 contours = Alg.get_all_contours(image=img)
 
-img_1 = np.zeros([100,100,1],dtype=np.uint8)
-img_2 = np.zeros([100,100,1],dtype=np.uint8)
-img_3 = np.zeros([100,100,1],dtype=np.uint8)
-img_4 = np.zeros([100,100,1],dtype=np.uint8)
+img_1 = np.zeros([100, 100, 1], dtype=np.uint8)
+img_2 = np.zeros([100, 100, 1], dtype=np.uint8)
+img_3 = np.zeros([100, 100, 1], dtype=np.uint8)
+img_4 = np.zeros([100, 100, 1], dtype=np.uint8)
 img_1.fill(255)
 img_2.fill(255)
 img_3.fill(255)
@@ -45,33 +43,27 @@ for cnt in contours:
     hull = cv.convexHull(cnt)
     cv.drawContours(img_2, [hull], 0, (0), 1)
 
-    approx = Alg.get_all_poly_points(img,0.02)
+    approx = Alg.get_all_poly_points(img, 0.02)
     cv.drawContours(img_3, [approx], 0, (0), 1)
 
-fig, axs = plt.subplots(nrows=2,ncols=2)
-axs[0,0].set_title("Poly Aprx. | Epsilon = 0.01")
-axs[0,1].set_title("Poly Aprx. | Epsilon = 0.02")
-axs[1,0].set_title("Convex Hull")
-axs[1,1].set_title("Original -> Not Contour")
-axs[0,0].imshow(img_1)
-axs[1,0].imshow(img_2)
-axs[0,1].imshow(img_3)
-axs[1,1].imshow(img)
+fig, axs = plt.subplots(nrows=2, ncols=2)
+axs[0, 0].set_title("Poly Aprx. | Epsilon = 0.01")
+axs[0, 1].set_title("Poly Aprx. | Epsilon = 0.02")
+axs[1, 0].set_title("Convex Hull")
+axs[1, 1].set_title("Original -> Not Contour")
+axs[0, 0].imshow(img_1)
+axs[1, 0].imshow(img_2)
+axs[0, 1].imshow(img_3)
+axs[1, 1].imshow(img)
 
 plt.show()
-
-
-
-
-
-
 
 
 # ********Adjusting all data to the same format*******
 # i = 999
 # for f in os.listdir("../dataset/cut"):
 #     f = os.path.join("../dataset/cut",f)
-    
+
 #     img = cv.imread(f)
 #     raw_gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 #     th, bin_img = cv.threshold(raw_gray_img, 1, 255, cv.THRESH_OTSU)
@@ -79,12 +71,10 @@ plt.show()
 #     _,contour,_ = cv.findContours(des,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
 #     for cnt in contour:
 #         cv.drawContours(des,[cnt],0,255,-1)
-    
+
 #     des = cv.bitwise_not(des)
 #     cv.imwrite("../dataset/cut/obj_" + str(i) + ".png", des)
 #     i -= 1
-
-
 
 
 # ******** Example of chain code histogram for input image *******
@@ -92,6 +82,6 @@ plt.show()
 img = cv.imread("../dataset/cut/obj_21.png")
 contours = Alg.get_all_contours(img)
 contours = np.array(contours)
-contours = contours.reshape(81,2)
+contours = contours.reshape(81, 2)
 histogram = Alg.get_chain_code_histogram(img)
 print(histogram)
