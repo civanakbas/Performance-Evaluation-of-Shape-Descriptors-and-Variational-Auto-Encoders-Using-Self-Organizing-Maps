9dd4f9e3d19e03c4c3e86d06034e07baa6cf9a35
civanakbas <civan.akbas@hotmail.com>
2022-05-31 03:02:39 +0300

    fixed cch

--- src/algorithms.py
+++ src/algorithms.py
@@ -141,12 +141,14 @@ class Algorithms:
         contours = self.get_all_contours(image)
         contours = np.array(contours)
         contours = contours.reshape(contours.shape[1], 2)
+        print(len(contours))
 
         for idx in range(len(contours) - 1):
             degree = self._calculate_compass_bearing(contours[idx], contours[idx + 1])
             histogram[self._check_direction(degree)] += 1
+            n_histogram = [item / len(contours) for item in histogram]
 
-        return histogram
+        return n_histogram
 
     def _find_angle(self, M1, M2):
         """Returns the angle between two slopes"""
